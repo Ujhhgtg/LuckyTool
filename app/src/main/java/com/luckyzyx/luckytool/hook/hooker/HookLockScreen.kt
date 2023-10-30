6fd851eaadb640f67a8c4f08e29d3bde01ea2039
luckyzyx <2813467705@qq.com>
2023-10-30 18:11:33 +0800

    - [适配] 移除锁屏关闭通知按钮 C13 C14

--- app/src/main/java/com/luckyzyx/luckytool/hook/hooker/HookLockScreen.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/hooker/HookLockScreen.kt
@@ -51,7 +51,7 @@ object HookLockScreen : YukiBaseHooker() {
 
         //移除锁屏关闭通知按钮
         if (prefs(ModulePrefs).getBoolean("remove_lock_screen_close_notification_button", false)) {
-            if (SDK < A13) loadHooker(RemoveLockScreenCloseNotificationButton)
+            loadHooker(RemoveLockScreenCloseNotificationButton)
         }
 
         //移除息屏音乐白名单
--- app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/RemoveLockScreenCloseNotificationButton.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/RemoveLockScreenCloseNotificationButton.kt
@@ -1,16 +1,19 @@
 package com.luckyzyx.luckytool.hook.scope.systemui
 
+import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
 
 object RemoveLockScreenCloseNotificationButton : YukiBaseHooker() {
     override fun onHook() {
         //Source NotificationPanelViewExt
-        "com.oplusos.systemui.notification.extend.NotificationPanelViewExt".toClass().apply {
-            method {
-                name = "setNotificationCloseButton"
-                paramCount = 1
-            }.hook { intercept() }
+        VariousClass(
+            "com.oplusos.systemui.notification.extend.NotificationPanelViewExt",  //C12 C13
+            "com.oplus.systemui.notification.extend.OplusNotificationCloseButtonImp"  //C14
+        ).toClass().apply {
+            method { name = "setNotificationCloseButton" }.hook {
+                intercept()
+            }
         }
     }
 }
