8160fbc16486615260898eec7c7a0dad61c01b00
luckyzyx <2813467705@qq.com>
2023-12-28 20:20:45 +0800

    - [优化] “允许不受信任的触摸”Hook逻辑

--- app/src/main/java/com/luckyzyx/luckytool/hook/scope/android/AllowUntrustedTouch.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scope/android/AllowUntrustedTouch.kt
@@ -2,20 +2,21 @@ package com.luckyzyx.luckytool.hook.scope.android
 
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
-import com.luckyzyx.luckytool.utils.A12
 import com.luckyzyx.luckytool.utils.A14
 import com.luckyzyx.luckytool.utils.ModulePrefs
 import com.luckyzyx.luckytool.utils.SDK
 
 object AllowUntrustedTouch : YukiBaseHooker() {
     override fun onHook() {
-        if (SDK < A12) return
         val isEnable = prefs(ModulePrefs).getBoolean("allow_untrusted_touch", false)
 
         //Source UntrustedTouchController
         "com.android.server.input.UntrustedTouchController".toClass().apply {
             method { name = "isOplusTrustedApp" }.hook {
                 if (isEnable) replaceToTrue()
+            }
+            method { name = "showTipsDialog" }.hook {
+                if (isEnable) intercept()
             }
         }
         //Source WindowStateExtImpl
