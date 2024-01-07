f3692285549b2a5418624a18e1b0150249bcb973
luckyzyx <2813467705@qq.com>
2024-01-07 17:50:00 +0800

    添加hidden-api-stub模块

---
+++ hidden-api-stub/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 34
+    namespace = "com.android.internal"
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin { jvmToolchain(17) }
+}
+
+dependencies {
+    implementation("androidx.annotation:annotation:1.7.1")
+}
