b5b44842faeaffbced732839e559fcc5bf276c4f
luckyzyx <2813467705@qq.com>
2024-03-11 21:10:19 +0800

    - [优化] 三方应用通话录音获取权限提示

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/audiomonitor/HookVoipRecorderService.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/audiomonitor/HookVoipRecorderService.kt
@@ -1,23 +1,25 @@
 package com.luckyzyx.luckytool.hook.scopes.audiomonitor
 
+import android.app.Service
 import android.os.SystemProperties
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
-import com.highcapable.yukihookapi.hook.log.YLog
+import com.luckyzyx.luckytool.utils.showToast
 import com.topjohnwu.superuser.Shell
 import com.topjohnwu.superuser.ShellUtils
 
 object HookVoipRecorderService : YukiBaseHooker() {
-    private const val prop = "ro.oplus.audio.voip_record_white_app_support"
+    private const val PROP = "ro.oplus.audio.voip_record_white_app_support"
     override fun onHook() {
         //Source OplusVoipRecorderService
         "com.oplus.audiomonitor.voiprecord.OplusVoipRecorderService".toClass().apply {
-            method { name = "onCreate" }.hook {
+            method { name = "onStartCommand" }.hook {
                 before {
-                    val isSupport = SystemProperties.getBoolean(prop, false)
+                    val service = instance<Service>()
+                    val isSupport = SystemProperties.getBoolean(PROP, false)
                     if (!isSupport) {
-                        if (Shell.getShell().isRoot) ShellUtils.fastCmd("setprop $prop true")
-                        else YLog.debug("AudioMonitor is no Root permission!")
+                        if (Shell.getShell().isRoot) ShellUtils.fastCmd("setprop $PROP true")
+                        else service.showToast("No Root!")
                     }
                 }
             }
