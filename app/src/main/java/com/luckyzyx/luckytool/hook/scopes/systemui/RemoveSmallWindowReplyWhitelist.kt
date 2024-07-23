17f98fba9958f3490dd58973fb4dbd63c60f9ca8
luckyzyx <2813467705@qq.com>
2024-07-23 18:25:30 +0800

    - [适配] 新版通知浮窗回复白名单 C15.0

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/systemui/RemoveSmallWindowReplyWhitelist.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/systemui/RemoveSmallWindowReplyWhitelist.kt
@@ -5,23 +5,43 @@ import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
 import com.luckyzyx.luckytool.utils.ModulePrefs
+import com.luckyzyx.luckytool.utils.getOSVersionCode
 
 object RemoveSmallWindowReplyWhitelist : YukiBaseHooker() {
     override fun onHook() {
-        var set =
-            prefs(ModulePrefs).getStringSet("set_small_window_reply_blacklist_list", ArraySet())
-        dataChannel.wait<Set<String>>("set_small_window_reply_blacklist_list") { set = it }
+        val osCode = getOSVersionCode
+        if (osCode >= 34) loadHooker(SmallWindowReplyWhitelist)
+        else loadHooker(SmallWindowReplyWhitelistV14)
+    }
 
-        //Source BaseNotificationContentInflater
-        VariousClass(
-            "com.oplusos.systemui.notification.base.BaseNotificationContentInflater", //C13
-            "com.oplus.systemui.statusbar.NotificationListenerExtImpl" //C14
-        ).toClass().apply {
-            method { name = "showSmallWindowReply" }.hook {
-                after {
-                    if (set.isEmpty()) return@after
-                    val packName = args().first().string()
-                    result = set.contains(packName).not()
+    object SmallWindowReplyWhitelist : YukiBaseHooker() {
+        override fun onHook() {
+            //Source HeadsUpToZoomUtils
+            "com.android.systemui.util.HeadsUpToZoomUtils".toClass().apply {
+                method { name = "isZoomValid" }.hook {
+                    replaceToTrue()
+                }
+            }
+        }
+    }
+
+    object SmallWindowReplyWhitelistV14 : YukiBaseHooker() {
+        override fun onHook() {
+            var set =
+                prefs(ModulePrefs).getStringSet("set_small_window_reply_blacklist_list", ArraySet())
+            dataChannel.wait<Set<String>>("set_small_window_reply_blacklist_list") { set = it }
+
+            //Source BaseNotificationContentInflater / NotificationListenerExtImpl
+            VariousClass(
+                "com.oplusos.systemui.notification.base.BaseNotificationContentInflater", //C13
+                "com.oplus.systemui.statusbar.NotificationListenerExtImpl" //C14
+            ).toClass().apply {
+                method { name = "showSmallWindowReply" }.hook {
+                    before {
+                        if (set.isEmpty()) return@before
+                        val packName = args().first().string()
+                        result = set.contains(packName).not()
+                    }
                 }
             }
         }
