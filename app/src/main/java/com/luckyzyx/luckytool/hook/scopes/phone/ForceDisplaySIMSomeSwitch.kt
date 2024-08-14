307f8ceb2375c1da744e92c971c4c9d8ae9daf61
luckyzyx <2813467705@qq.com>
2024-08-14 22:23:03 +0800

    优化VoLTE与首选网络类型选项hook逻辑

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/phone/ForceDisplaySIMSomeSwitch.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/phone/ForceDisplaySIMSomeSwitch.kt
@@ -1,7 +1,10 @@
 package com.luckyzyx.luckytool.hook.scopes.phone
 
+import android.content.DialogInterface
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.type.android.ContextClass
+import com.highcapable.yukihookapi.hook.type.android.ViewClass
 import com.highcapable.yukihookapi.hook.type.java.BooleanType
 import com.highcapable.yukihookapi.hook.type.java.IntType
 import com.highcapable.yukihookapi.hook.type.java.StringClass
@@ -9,6 +12,7 @@ import com.highcapable.yukihookapi.hook.type.java.UnitType
 import com.luckyzyx.luckytool.utils.DexkitUtils.checkDataList
 import com.luckyzyx.luckytool.utils.ModulePrefs
 import org.luckypray.dexkit.DexKitBridge
+import org.luckypray.dexkit.query.enums.StringMatchType
 
 class ForceDisplaySIMSomeSwitch(val dexKitBridge: DexKitBridge) : YukiBaseHooker() {
     override fun onHook() {
@@ -16,54 +20,81 @@ class ForceDisplaySIMSomeSwitch(val dexKitBridge: DexKitBridge) : YukiBaseHooker
         val preferredNetwork =
             prefs(ModulePrefs).getBoolean("force_display_preferred_network_type", false)
 
-        //Source OplusSimInfoActivity changeVolteSwitchConfig
-        dexKitBridge.findMethod {
+        dexKitBridge.findClass {
             matcher {
-                paramTypes(IntType, BooleanType, StringClass)
-                returnType(UnitType)
-                usingNumbers(1, 2, 3, 4, 7)
-                usingStrings("changeVolteSwitchConfig", "SIMS_OplusSimInfoActivity")
-                addUsingField {
-                    type("com.coui.appcompat.preference.COUISwitchPreference")
-                }
+                className(
+                    "com.android.simsettings.activity.OplusSimInfoActivity",
+                    StringMatchType.Contains
+                )
+                addFieldForType(ContextClass)
+                addFieldForType(StringClass)
+                addFieldForType(BooleanType)
+                addFieldForType(ViewClass)
+                addFieldForType(DialogInterface.OnClickListener::class.java)
             }
         }.apply {
-            checkDataList("ForceDisplaySomeSwitch changeVolteSwitchConfig")
-            single().className.toClass().apply {
-                method {
-                    name = single().methodName
-                    param(IntType, BooleanType, StringClass)
-                }.hook {
-                    before {
-                        if (!volteCall) return@before
-                        val type = args().first().int()
-                        if (type == 1) args(1).setTrue()
+            checkDataList("ForceDisplaySIMSomeSwitch Clazz")
+
+            //Source OplusSimInfoActivity changeVolteSwitchConfig
+            findMethod {
+                matcher {
+                    paramTypes(IntType, null, null)
+                    returnType(UnitType)
+                    usingNumbers(1, 2, 3, 4, 7)
+                    usingStrings("changeVolteSwitchConfig", "SIMS_OplusSimInfoActivity")
+                    addUsingField {
+                        type("com.coui.appcompat.preference.COUISwitchPreference")
+                    }
+                }
+            }.apply {
+                checkDataList("ForceDisplaySomeSwitch changeVolteSwitchConfig")
+                single().className.toClass().apply {
+                    method {
+                        name = single().methodName
+                        param {
+                            it[0] == IntType && it.contains(BooleanType) && it.contains(StringClass)
+                        }
+                        paramCount = 3
+                    }.hook {
+                        before {
+                            if (!volteCall) return@before
+                            val type = args().first().int()
+                            val bool = args.find { it is Boolean } ?: return@before
+                            val index = args.indexOf(bool).takeIf { it != -1 } ?: return@before
+                            if (type == 1) args(index).setTrue()
+                        }
                     }
                 }
             }
-        }
-        //Source OplusSimInfoActivity changeNetworkModeConfig
-        dexKitBridge.findMethod {
-            matcher {
-                paramTypes(IntType, BooleanType, StringClass)
-                returnType(UnitType)
-                usingNumbers(1, 2, 5)
-                usingStrings("changeNetworkModeConfig", "SIMS_OplusSimInfoActivity")
-                addUsingField {
-                    type("com.coui.appcompat.preference.COUIJumpPreference")
+
+            //Source OplusSimInfoActivity changeNetworkModeConfig
+            findMethod {
+                matcher {
+                    paramTypes(IntType, null, null)
+                    returnType(UnitType)
+                    usingNumbers(1, 2, 5)
+                    usingStrings("changeNetworkModeConfig", "SIMS_OplusSimInfoActivity")
+                    addUsingField {
+                        type("com.coui.appcompat.preference.COUIJumpPreference")
+                    }
                 }
-            }
-        }.apply {
-            checkDataList("ForceDisplaySomeSwitch changeNetworkModeConfig")
-            single().className.toClass().apply {
-                method {
-                    name = single().methodName
-                    param(IntType, BooleanType, StringClass)
-                }.hook {
-                    before {
-                        if (!preferredNetwork) return@before
-                        val type = args().first().int()
-                        if (type == 1) args(1).setTrue()
+            }.apply {
+                checkDataList("ForceDisplaySomeSwitch changeNetworkModeConfig")
+                single().className.toClass().apply {
+                    method {
+                        name = single().methodName
+                        param {
+                            it[0] == IntType && it.contains(BooleanType) && it.contains(StringClass)
+                        }
+                        paramCount = 3
+                    }.hook {
+                        before {
+                            if (!preferredNetwork) return@before
+                            val type = args().first().int()
+                            val bool = args.find { it is Boolean } ?: return@before
+                            val index = args.indexOf(bool).takeIf { it != -1 } ?: return@before
+                            if (type == 1) args(index).setTrue()
+                        }
                     }
                 }
             }
