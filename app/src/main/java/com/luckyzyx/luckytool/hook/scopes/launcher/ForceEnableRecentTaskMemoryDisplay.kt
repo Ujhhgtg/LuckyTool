457f5999f36b5a0ac244aebbd942b6cc8247907a
luckyzyx <2813467705@qq.com>
2024-12-01 21:33:50 +0800

    - [适配] 强制启用最近任务内存显示 2

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/launcher/ForceEnableRecentTaskMemoryDisplay.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/launcher/ForceEnableRecentTaskMemoryDisplay.kt
@@ -1,11 +1,7 @@
 package com.luckyzyx.luckytool.hook.scopes.launcher
 
-import android.content.Context
-import android.provider.Settings
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.constructor
-import com.highcapable.yukihookapi.hook.factory.field
-import com.highcapable.yukihookapi.hook.type.android.ContextClass
+import com.highcapable.yukihookapi.hook.factory.method
 import com.joom.paranoid.Obfuscate
 
 @Obfuscate
@@ -13,17 +9,11 @@ object ForceEnableRecentTaskMemoryDisplay : YukiBaseHooker() {
     override fun onHook() {
         //Source MemoryInfoManager
         "com.oplus.quickstep.memory.MemoryInfoManager".toClass().apply {
-            constructor { param(ContextClass) }.hook {
-                before {
-                    val context = args().first().cast<Context>() ?: return@before
-                    Settings.Secure.putInt(
-                        context.contentResolver, "display_memory_information_recent_task", 1
-                    )
-                    Settings.Secure.putInt(context.contentResolver, "allow_memory_info_display", 1)
-                }
-                after {
-                    field { name = "mAllowMemoryInfoDisplay" }.get(instance).setTrue()
-                }
+            method { name = "isAllowMemoryInfoDisplay" }.hook {
+                replaceToTrue()
+            }
+            method { name = "needMemoryDetail" }.hook {
+                replaceToTrue()
             }
         }
     }
