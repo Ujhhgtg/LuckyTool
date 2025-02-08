f7dfc698431e6f90b978d5e2eb026e2b4e940dfc
luckyzyx <2813467705@qq.com>
2025-02-09 00:20:08 +0800

    优化Apk安装页面异常问题

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/packageinstaller/FixInstallButtonDisplayException.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/packageinstaller/FixInstallButtonDisplayException.kt
@@ -1,15 +1,24 @@
 package com.luckyzyx.luckytool.hook.scopes.packageinstaller
 
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.factory.constructor
 import com.highcapable.yukihookapi.hook.factory.field
+import com.highcapable.yukihookapi.hook.factory.hasField
 import com.highcapable.yukihookapi.hook.factory.method
 import com.joom.paranoid.Obfuscate
+import java.security.SecureRandom
 
 @Obfuscate
 object FixInstallButtonDisplayException : YukiBaseHooker() {
     override fun onHook() {
         //Source ConfusedButton
         "com.android.packageinstaller.oplus.view.ConfusedButton".toClass().apply {
+            val hasRamdom = hasField { type = SecureRandom::class.java }
+            if (hasRamdom) constructor().hookAll {
+                after {
+                    field { type = SecureRandom::class.java }.get(instance).set(SecureRandom())
+                }
+            }
             method { name = "getText" }.hook {
                 before {
                     field { name = "mIsCtsTesting" }.get(instance).setTrue()
