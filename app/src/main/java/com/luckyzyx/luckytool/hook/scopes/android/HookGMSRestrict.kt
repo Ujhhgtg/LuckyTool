4b10919441b2c021b46fbb0507621b559d39290f
luckyzyx <2813467705@qq.com>
2025-03-13 15:11:26 +0800

    优化GMS SDK hook逻辑

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/android/HookGMSRestrict.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/android/HookGMSRestrict.kt
@@ -1,36 +1,57 @@
 package com.luckyzyx.luckytool.hook.scopes.android
 
-import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
 import com.luckyzyx.luckytool.utils.ModulePrefs
+import com.luckyzyx.luckytool.utils.getOSVersionCode
 import org.lsposed.lsparanoid.Obfuscate
 
 @Obfuscate
 object HookGMSRestrict : YukiBaseHooker() {
     override fun onHook() {
+        val osCode = getOSVersionCode
         val isEnable = prefs(ModulePrefs).getBoolean("remove_gms_usage_restrictions", false)
         if (!isEnable) return
 
-        //Source OplusBgSceneManager -> google_restric_info
-        VariousClass(
-            "com.android.server.am.OplusHansManager\$HansConfig", //C12 C13
-            "com.android.server.hans.scene.OplusBgSceneManager" //C14 C15
-        ).toClass().apply {
-            method { name = "setGmsRestricted" }.hook {
-                before {
-                    args().first().setFalse()
-                }
-            }
-            method { name = "isGmsRestricted" }.hook {
-                replaceToFalse()
-            }
-        }
+        if (osCode >= 30) loadHooker(GMSRestrict)
+        else loadHooker(GMSRestrictV13)
 
         //Source OplusAppStartupManager -> OplusStartupStrategy -> google_restric_info
         "com.android.server.am.OplusAppStartupManager\$OplusStartupStrategy".toClass().apply {
             method { name = "isGoogleRestricInfoOn" }.hook {
                 replaceToFalse()
+            }
+        }
+    }
+
+    object GMSRestrict : YukiBaseHooker() {
+        override fun onHook() {
+            //Source OplusBgSceneManager -> google_restric_info
+            "com.android.server.hans.scene.OplusBgSceneManager".toClass().apply {
+                method { name = "setGmsRestricted" }.hook {
+                    before {
+                        args().first().setFalse()
+                    }
+                }
+                method { name = "isGmsRestricted" }.hook {
+                    replaceToFalse()
+                }
+            }
+        }
+    }
+
+    object GMSRestrictV13 : YukiBaseHooker() {
+        override fun onHook() {
+            //Source OplusHansManager -> HansConfig -> google_restric_info
+            "com.android.server.am.OplusHansManager\$HansConfig".toClass().apply {
+                method { name = "setGmsRestricted" }.hook {
+                    before {
+                        args().first().setFalse()
+                    }
+                }
+                method { name = "isGmsRestricted" }.hook {
+                    replaceToFalse()
+                }
             }
         }
     }
