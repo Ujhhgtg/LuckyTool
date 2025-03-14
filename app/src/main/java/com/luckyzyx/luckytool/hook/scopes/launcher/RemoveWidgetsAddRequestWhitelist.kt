a29b8a666304def2031424cfd0ca8350da3d2a97
luckyzyx <2813467705@qq.com>
2025-03-14 13:08:05 +0800

    优化桌面小组件hook点 C14

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/launcher/RemoveWidgetsAddRequestWhitelist.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/launcher/RemoveWidgetsAddRequestWhitelist.kt
@@ -1,15 +1,22 @@
 package com.luckyzyx.luckytool.hook.scopes.launcher
 
+import android.content.pm.LauncherApps
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.type.android.ContextClass
 import org.lsposed.lsparanoid.Obfuscate
 
 @Obfuscate
 object RemoveWidgetsAddRequestWhitelist : YukiBaseHooker() {
     override fun onHook() {
+        //Source WidgetControlHelper
+        "com.android.launcher3.widget.WidgetControlHelper".toClassOrNull() ?: return
         //Source AddItemActivity
         "com.android.launcher3.dragndrop.AddItemActivity".toClass().apply {
-            method { name = "isAllowedAddWidget" }.hook{
+            method {
+                name = "isAllowedAddWidget"
+                param(ContextClass, LauncherApps.PinItemRequest::class.java)
+            }.hook {
                 replaceToTrue()
             }
         }
