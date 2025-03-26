878c45132ecce639c82ead8bb8cad200dc8b927e
luckyzyx <2813467705@qq.com>
2025-03-26 18:38:05 +0800

    重新修复packageinstaller异常崩溃问题

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/packageinstaller/FixInstallButtonDisplayException.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/packageinstaller/FixInstallButtonDisplayException.kt
@@ -1,9 +1,7 @@
 package com.luckyzyx.luckytool.hook.scopes.packageinstaller
 
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.constructor
 import com.highcapable.yukihookapi.hook.factory.field
-import com.highcapable.yukihookapi.hook.factory.hasField
 import com.highcapable.yukihookapi.hook.factory.method
 import org.lsposed.lsparanoid.Obfuscate
 import java.security.SecureRandom
@@ -13,15 +11,31 @@ object FixInstallButtonDisplayException : YukiBaseHooker() {
     override fun onHook() {
         //Source ConfusedButton
         "com.android.packageinstaller.oplus.view.ConfusedButton".toClass().apply {
-            val hasRamdom = hasField { type = SecureRandom::class.java }
-            if (hasRamdom) constructor().hookAll {
-                after {
-                    field { type = SecureRandom::class.java }.get(instance).set(SecureRandom())
+            method { name = "getAccessibilityViewId" }.hook {
+                before {
+                    field { name = "mIsCtsTesting" }.get(instance).setTrue()
+                    field { name = "mRandom" }.get(instance).set(SecureRandom())
                 }
             }
             method { name = "getText" }.hook {
                 before {
                     field { name = "mIsCtsTesting" }.get(instance).setTrue()
+                    field { name = "mRandom" }.get(instance).set(SecureRandom())
+                }
+            }
+        }
+        //Source ConfusedTextView
+        "com.android.packageinstaller.oplus.view.ConfusedTextView".toClass().apply {
+            method { name = "getAccessibilityViewId" }.hook {
+                before {
+                    field { name = "mIsCtsTesting" }.get(instance).setTrue()
+                    field { name = "mRandom" }.get(instance).set(SecureRandom())
+                }
+            }
+            method { name = "getText" }.hook {
+                before {
+                    field { name = "mIsCtsTesting" }.get(instance).setTrue()
+                    field { name = "mRandom" }.get(instance).set(SecureRandom())
                 }
             }
         }
