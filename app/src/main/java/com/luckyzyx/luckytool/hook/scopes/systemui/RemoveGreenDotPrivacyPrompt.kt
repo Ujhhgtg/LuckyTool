c8569e7611e8a0490d0d708c7d8900c12f8d1f31
luckyzyx <2813467705@qq.com>
2025-05-07 22:26:31 +0800

    - [更改] 更换状态栏隐私绿点提示Hook点

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/systemui/RemoveGreenDotPrivacyPrompt.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/systemui/RemoveGreenDotPrivacyPrompt.kt
@@ -2,35 +2,81 @@ package com.luckyzyx.luckytool.hook.scopes.systemui
 
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.type.android.ViewClass
+import com.luckyzyx.luckytool.utils.getOSVersionCode
 import org.lsposed.lsparanoid.Obfuscate
-import com.luckyzyx.luckytool.utils.A14
-import com.luckyzyx.luckytool.utils.SDK
 
 @Obfuscate
 object RemoveGreenDotPrivacyPrompt : YukiBaseHooker() {
     override fun onHook() {
-        //Source PrivacyDotViewController
-        "com.android.systemui.statusbar.events.PrivacyDotViewController".toClass().apply {
-            hookMethod()
-        }
-        //二次处理
-        //Source OplusPrivacyDotViewController
-        "com.oplusos.systemui.statusbar.events.OplusPrivacyDotViewController".toClassOrNull()
-            ?.hookMethod()
+        val osCode = getOSVersionCode
+        if (osCode >= 34) loadHooker(GreenDotPrivacyPrompt)
+        else if (osCode >= 30) loadHooker(GreenDotPrivacyPromptV14)
+        else loadHooker(GreenDotPrivacyPromptV13)
+    }
 
-        if (SDK < A14) return
-        //Source OplusPrivacyDotViewController
-        "com.oplus.systemui.privacy.OplusPrivacyDotViewController".toClass().apply {
-            hookMethod()
+    @Obfuscate
+    object GreenDotPrivacyPrompt : YukiBaseHooker() {
+        override fun onHook() {
+            //Source ScreenDecorations
+            "com.android.systemui.ScreenDecorations".toClass().apply {
+                method {
+                    name = "updateOverlayWindowVisibilityIfViewExists"
+                    param(ViewClass)
+                }.hook {
+                    intercept()
+                }
+            }
         }
     }
 
-    private fun Class<*>.hookMethod() {
-        method { name = "showDotView";paramCount = 2 }.hook {
-            intercept()
+    @Obfuscate
+    object GreenDotPrivacyPromptV14 : YukiBaseHooker() {
+        override fun onHook() {
+            //Source ScreenDecorations
+            "com.android.systemui.ScreenDecorations".toClass().apply {
+                method {
+                    name = "updateOverlayWindowVisibilityIfViewExists"
+                    param(ViewClass)
+                }.hook {
+                    intercept()
+                }
+            }
+            //二次处理
+            //Source OplusPrivacyDotViewController
+            "com.oplus.systemui.privacy.OplusPrivacyDotViewController".toClass().apply {
+                method { name = "showDotView";paramCount = 2 }.hook {
+                    intercept()
+                }
+                method { name = "updateDesignatedCorner";paramCount = 2 }.hook {
+                    intercept()
+                }
+            }
         }
-        method { name = "updateDesignatedCorner";paramCount = 2 }.hook {
-            intercept()
+    }
+
+    @Obfuscate
+    object GreenDotPrivacyPromptV13 : YukiBaseHooker() {
+        override fun onHook() {
+            //Source PrivacyDotViewController
+            "com.android.systemui.statusbar.events.PrivacyDotViewController".toClass().apply {
+                method { name = "showDotView";paramCount = 2 }.hook {
+                    intercept()
+                }
+                method { name = "updateDesignatedCorner";paramCount = 2 }.hook {
+                    intercept()
+                }
+            }
+            //二次处理
+            //Source OplusPrivacyDotViewController
+            "com.oplusos.systemui.statusbar.events.OplusPrivacyDotViewController".toClass().apply {
+                method { name = "showDotView";paramCount = 2 }.hook {
+                    intercept()
+                }
+                method { name = "updateDesignatedCorner";paramCount = 2 }.hook {
+                    intercept()
+                }
+            }
         }
     }
 }
