a3a5e391e46006f9a202dd898a80b02acc0c798f
luckyzyx <2813467705@qq.com>
2025-05-07 23:16:44 +0800

    - [优化] 优化模块本地权限请求逻辑

--- app/src/main/java/com/luckyzyx/luckytool/utils/PermissionUtils.kt
+++ app/src/main/java/com/luckyzyx/luckytool/utils/PermissionUtils.kt
@@ -1,50 +1,49 @@
 package com.luckyzyx.luckytool.utils
 
 import android.content.Context
-import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.highcapable.betterandroid.ui.extension.view.layoutInflater
 import com.hjq.permissions.Permission
 import com.hjq.permissions.XXPermissions
 import com.luckyzyx.luckytool.R
 import com.luckyzyx.luckytool.databinding.DialogPermissionLayoutBinding
+import com.luckyzyx.luckytool.ui.activity.MainActivity
 import org.lsposed.lsparanoid.Obfuscate
 
 @Obfuscate
 class PermissionUtils(val context: Context) {
 
     val binding = DialogPermissionLayoutBinding.inflate(context.layoutInflater)
-    private val dialogBuilder = MaterialAlertDialogBuilder(context, dialogCentered).apply {
+    private val dialog = MaterialAlertDialogBuilder(context, dialogCentered).apply {
         setCancelable(false)
         setView(binding.root)
-    }
-    private var dialog: AlertDialog? = null
+    }.create()
 
     private var notifyStatus = false
     private var storageStatus = false
     private var appListStatus = false
     private var installAppStatus = false
 
-    fun init(): Boolean {
+    private var allowDismiss = false
+
+    fun init() {
         notifyStatus = refreshNotifyChip()
         storageStatus = refreshStorageChip()
         appListStatus = refreshAppListChip()
         installAppStatus = refreshInstallAppChip()
 
-        val status = notifyStatus && storageStatus && appListStatus && installAppStatus
-        if (status) {
-            dialogBuilder.setOnDismissListener(null)
-            dialog?.setOnDismissListener(null)
-            dialog?.dismiss()
-        }
-        return status
+        allowDismiss = notifyStatus && storageStatus && appListStatus && installAppStatus
+        if (allowDismiss) dialog.setOnDismissListener(null)
+        dialog.dismiss()
     }
 
     fun start() {
-        if (init()) return
-        dialogBuilder.setOnDismissListener { context.exitModule() }
-        dialog?.setOnDismissListener { context.exitModule() }
-        dialog = dialogBuilder.show()
+        init()
+        if (allowDismiss) return
+        dialog.setOnDismissListener {
+            (context as MainActivity).restart()
+        }
+        dialog.show()
     }
 
     private fun refreshNotifyChip(): Boolean {
