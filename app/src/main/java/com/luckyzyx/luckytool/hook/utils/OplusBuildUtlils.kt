77226504f0254b6591190f4320aca7ebb546bed7
luckyzyx <2813467705@qq.com>
2025-05-08 22:17:52 +0800

    添加OS V15.0.2 V16

--- app/src/main/java/com/luckyzyx/luckytool/hook/utils/OplusBuildUtlils.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/utils/OplusBuildUtlils.kt
@@ -10,6 +10,7 @@ import org.lsposed.lsparanoid.Obfuscate
 class OplusBuildUtlils(val classLoader: ClassLoader? = null) {
 
     val clazz = "com.oplus.os.OplusBuild".toClass(classLoader)
+    val osdkVersionCodesClazz = "com.oplus.os.OplusBuild\$OsdkVersionCodes".toClass(classLoader)
 
     /**
      * 23 -> c12
@@ -25,7 +26,7 @@ class OplusBuildUtlils(val classLoader: ClassLoader? = null) {
         "V1.0", "V1.2", "V1.4", "V2.0", "V2.1", "V3.0", "V3.1", "V3.2", "V5.0", "V5.1",
         "V5.2", "V6.0", "V6.1", "V6.2", "V6.7", "V7", "V7.1", "V7.2", "V11", "V11.1",
         "V11.2", "V11.3", "V12", "V12.1", "V12.2", "V13", "V13.1", "V13.1.1", "V13.2", "V14.0",
-        "V14.0.1", "V14.0.2", "V14.1.0", "V15.0.0", "V15.0.1",
+        "V14.0.1", "V14.0.2", "V14.1.0", "V15.0.0", "V15.0.1", "V15.0.2", "V16",
         "null"
     )
 
@@ -34,5 +35,7 @@ class OplusBuildUtlils(val classLoader: ClassLoader? = null) {
     val getOSVersionCode get() = clazz.method { name = "getOplusOSVERSION" }.get().invoke<Int>()
 
     val getOSVersionName get() = getOSVersionCode?.let { getOSVersions?.get(it - 1) }
+
+    val getOsdkVersionCodes get() = osdkVersionCodesClazz.fields.map { it.name }.sorted()
 
 }
