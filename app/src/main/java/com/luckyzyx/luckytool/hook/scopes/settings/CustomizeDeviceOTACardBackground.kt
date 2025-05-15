9797af951332a21d99ecb6a2f9a7785c464d0f3c
luckyzyx <2813467705@qq.com>
2025-05-15 22:33:04 +0800

    - [适配] 自定义OTA卡片背景 C15.0.1

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/settings/CustomizeDeviceOTACardBackground.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/settings/CustomizeDeviceOTACardBackground.kt
@@ -1,7 +1,10 @@
 package com.luckyzyx.luckytool.hook.scopes.settings
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.graphics.BitmapFactory
 import android.view.View
+import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.core.view.children
@@ -9,14 +12,15 @@ import androidx.core.view.isVisible
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.current
 import com.highcapable.yukihookapi.hook.factory.method
-import org.lsposed.lsparanoid.Obfuscate
 import com.luckyzyx.luckytool.utils.ModulePrefs
 import com.luckyzyx.luckytool.utils.dp
 import com.luckyzyx.luckytool.utils.getOSVersionCode
+import org.lsposed.lsparanoid.Obfuscate
 
 @Obfuscate
 object CustomizeDeviceOTACardBackground : YukiBaseHooker() {
 
+    @SuppressLint("DiscouragedApi")
     override fun onHook() {
         val osCode = getOSVersionCode
         val backgroundPath =
@@ -33,6 +37,13 @@ object CustomizeDeviceOTACardBackground : YukiBaseHooker() {
                     val itemView = holder.current().field { name = "itemView";superClass() }
                         .cast<View>() ?: return@after
                     if (itemView is RelativeLayout) {
+                        val topId = itemView.resources.getIdentifier(
+                            "about_device_top_bg", "id",
+                            this@CustomizeDeviceOTACardBackground.packageName
+                        )
+                        itemView.findViewById<ImageView>(topId)?.let {
+                            itemView.removeView(it)
+                        }
                         val bitmap = BitmapFactory.decodeFile(backgroundPath) ?: return@after
                         val drawableFactory = RoundedBitmapDrawableFactory.create(
                             itemView.resources, bitmap
@@ -50,12 +61,24 @@ object CustomizeDeviceOTACardBackground : YukiBaseHooker() {
 
         if (osCode < 34 || !applySharePage) return
 
-        //Source ShareAboutPhoneActivity
+        //Source ShareAboutPhoneActivity parent_relativeLayout
         "com.oplus.settings.feature.deviceinfo.aboutphone.ShareAboutPhoneActivity".toClass().apply {
             method { name = "updateOsVersion" }.hook {
                 after {
-                    val relativeLayout = method { name = "getMLogoContent" }.get(instance)
-                        .invoke<RelativeLayout>() ?: return@after
+                    val activity = instance<Activity>()
+                    val viewId = activity.resources.getIdentifier(
+                        "parent_relativeLayout", "id",
+                        this@CustomizeDeviceOTACardBackground.packageName
+                    )
+                    val relativeLayout = activity.findViewById<RelativeLayout>(viewId)
+                        ?: return@after
+                    val topId = activity.resources.getIdentifier(
+                        "about_device_top_bg", "id",
+                        this@CustomizeDeviceOTACardBackground.packageName
+                    )
+                    activity.findViewById<ImageView>(topId)?.let {
+                        relativeLayout.removeView(it)
+                    }
                     val bitmap = BitmapFactory.decodeFile(backgroundPath) ?: return@after
                     val drawableFactory = RoundedBitmapDrawableFactory.create(
                         relativeLayout.resources, bitmap
