31eecc64b6b918fff52b9403cf7b29ffbc853321
luckyzyx <2813467705@qq.com>
2025-05-19 00:16:39 +0800

    - [适配] 控制中心响铃状态切换磁贴 C15.0.1

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/systemui/ForceDisplayOfRingingStatusToggleTiles.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/systemui/ForceDisplayOfRingingStatusToggleTiles.kt
@@ -2,10 +2,12 @@ package com.luckyzyx.luckytool.hook.scopes.systemui
 
 import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.factory.constructor
 import com.highcapable.yukihookapi.hook.factory.current
 import com.highcapable.yukihookapi.hook.factory.field
-import com.highcapable.yukihookapi.hook.factory.hasMethod
 import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.type.java.AnyClass
+import com.highcapable.yukihookapi.hook.type.java.StringClass
 import org.lsposed.lsparanoid.Obfuscate
 
 @Obfuscate
@@ -18,32 +20,11 @@ object ForceDisplayOfRingingStatusToggleTiles : YukiBaseHooker() {
         ).toClass().apply {
             method { name = "getDfltUnsupportedTileString" }.hook {
                 after {
-                    val list = result<List<String>>()?.toMutableList()?.apply {
+                    val list = result<List<String>>() ?: return@after
+                    val new = list.toMutableList().apply {
                         remove("ringermode")
-                    } ?: return@after
-                    result = ArrayList<String>(list)
-                }
-            }
-        }
-
-        //Source OplusQSFactoryImpl
-        VariousClass(
-            "com.oplusos.systemui.qs.qstileimpl.OplusQSFactoryImpl",  //C13
-            "com.oplus.systemui.qs.qstileimpl.OplusQSFactoryImpl",  //C14
-            "com.oplus.systemui.qs.tileimpl.OplusQSFactoryImpl"  //C15
-        ).toClass().apply {
-            if (!hasMethod { name = "createTileInternal" }) return
-            method { name = "createTileInternal" }.hook {
-                before {
-                    val key = args().first().string()
-                    if (key == "ringermode") {
-                        val provider = field {
-//                            name = "mFlavorOneRingerModeTileProvider"
-//                            name = "mThreeStageRingerModeTileProvider"
-                            name { it.contains("RingerModeTileProvider") }
-                        }.get(instance).any() ?: return@before
-                        result = provider.current().method { name = "get" }.call()
                     }
+                    result = ArrayList(new)
                 }
             }
         }
@@ -54,8 +35,37 @@ object ForceDisplayOfRingingStatusToggleTiles : YukiBaseHooker() {
             "com.oplus.systemui.qs.tiles.FlavorOneRingerModeTile",  //C14.0 C14.0.1
             "com.oplus.systemui.qs.tiles.ThreeStageRingerModeTile" //C14.1
         ).toClass().apply {
+            //修复实例为Null无法获取spec问题
+            constructor().hook {
+                after {
+                    method { name = "setTileSpec";superClass() }.get(instance).call("ringermode")
+                }
+            }
             method { name = "isAvailable";superClass(true) }.hook {
                 replaceToTrue()
+            }
+        }
+
+        //Source OplusQSFactoryImpl
+        VariousClass(
+            "com.oplusos.systemui.qs.qstileimpl.OplusQSFactoryImpl",  //C13
+            "com.oplus.systemui.qs.qstileimpl.OplusQSFactoryImpl",  //C14
+            "com.oplus.systemui.qs.tileimpl.OplusQSFactoryImpl"  //C15
+        ).toClass().apply {
+            method { name = "createTile";param(StringClass) }.hook {
+                before {
+                    val key = args().first().string()
+                    if (key == "ringermode") {
+                        val provider = field {
+//                            name = "mFlavorOneRingerModeTileProvider"
+//                            name = "mThreeStageRingerModeTileProvider"
+                            name { it.contains("RingerModeTileProvider") }
+                        }.get(instance).any() ?: return@before
+                        result = provider.current().method {
+                            name = "get";returnType = AnyClass
+                        }.call()
+                    }
+                }
             }
         }
     }
