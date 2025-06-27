12bf3be7123fe7836a1d1c0c8d0efade6105b378
luckyzyx <2813467705@qq.com>
2025-06-28 00:02:28 +0800

    迁移KavaRef API directui

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/directui/RemoveTouchAppRecommendCard.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/directui/RemoveTouchAppRecommendCard.kt
@@ -1,15 +1,15 @@
 package com.luckyzyx.luckytool.hook.scopes.directui
 
+import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.method
 import org.lsposed.lsparanoid.Obfuscate
 
 @Obfuscate
 object RemoveTouchAppRecommendCard : YukiBaseHooker() {
     override fun onHook() {
         //Source DirectUIMainViewMode -> AppBean
-        "com.coloros.directui.repository.datasource.AppBean".toClass().apply {
-            method { name = "toCardUIInfo" }.hook {
+        "com.coloros.directui.repository.datasource.AppBean".toClass().resolve().apply {
+            firstMethod { name = "toCardUIInfo" }.hook {
                 intercept()
             }
         }
