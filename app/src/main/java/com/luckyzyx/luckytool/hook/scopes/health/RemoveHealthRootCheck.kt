45b27f6dc2176cde4ecd5c436fd82507313223b1
luckyzyx <2813467705@qq.com>
2025-06-28 23:23:58 +0800

    迁移KavaRef API health

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/health/RemoveHealthRootCheck.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/health/RemoveHealthRootCheck.kt
@@ -1,16 +1,16 @@
 package com.luckyzyx.luckytool.hook.scopes.health
 
+import android.app.Activity
+import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.method
-import com.highcapable.yukihookapi.hook.type.android.ActivityClass
 import org.lsposed.lsparanoid.Obfuscate
 
 @Obfuscate
 object RemoveHealthRootCheck : YukiBaseHooker() {
     override fun onHook() {
         //Source SafetyCheckManager
-        "com.heytap.health.safety.safetycheck.SafetyCheckManager".toClass().apply {
-            method { param(ActivityClass) }.hook {
+        "com.heytap.health.safety.safetycheck.SafetyCheckManager".toClass().resolve().apply {
+            firstMethod { parameters(Activity::class) }.hook {
                 intercept()
             }
         }
