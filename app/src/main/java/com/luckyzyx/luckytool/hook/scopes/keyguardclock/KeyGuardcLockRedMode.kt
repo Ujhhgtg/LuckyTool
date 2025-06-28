949dd2ca8296fba8456db2d75a1a4afe1937a39e
luckyzyx <2813467705@qq.com>
2025-06-28 23:25:07 +0800

    迁移KavaRef API keyguardclock

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/keyguardclock/KeyGuardcLockRedMode.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/keyguardclock/KeyGuardcLockRedMode.kt
@@ -1,14 +1,10 @@
 package com.luckyzyx.luckytool.hook.scopes.keyguardclock
 
+import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.field
-import com.highcapable.yukihookapi.hook.type.java.BooleanType
-import com.highcapable.yukihookapi.hook.type.java.CharSequenceClass
-import com.highcapable.yukihookapi.hook.type.java.IntType
-import com.highcapable.yukihookapi.hook.type.java.UnitType
-import org.lsposed.lsparanoid.Obfuscate
 import com.luckyzyx.luckytool.utils.DexkitUtils.checkDataList
 import com.luckyzyx.luckytool.utils.ModulePrefs
+import org.lsposed.lsparanoid.Obfuscate
 import org.luckypray.dexkit.DexKitBridge
 
 @Obfuscate
@@ -19,31 +15,34 @@ class KeyGuardcLockRedMode(val dexKitBridge: DexKitBridge) : YukiBaseHooker() {
         //Source CustomizedTextView -> BrandUtils
         dexKitBridge.findClass {
             matcher {
-                addFieldForType(BooleanType)
+                addFieldForType(Boolean::class.java)
                 usingStrings("ro.oplus.image.system_ext.brand", "ro.oplus.image.system_ext.area")
             }
         }.apply {
             checkDataList("KeyGuardcLockRedMode Clazz")
             findField {
                 matcher {
-                    type(BooleanType)
+                    type(Boolean::class.java)
                     addReadMethod {
-                        paramTypes(IntType, IntType)
-                        returnType(UnitType)
+                        paramTypes(Int::class.java, Int::class.java)
+                        returnType(Void.TYPE)
                     }
                     addReadMethod {
-                        paramTypes(CharSequenceClass)
-                        returnType(UnitType)
+                        paramTypes(CharSequence::class.java)
+                        returnType(Void.TYPE)
                     }
                     addReadMethod {
                         paramCount(0)
-                        returnType(UnitType)
+                        returnType(Void.TYPE)
                     }
                 }
             }.apply {
                 checkDataList("KeyGuardcLockRedMode Field")
-                single().className.toClass(initialize = true).apply {
-                    field { name = single().fieldName;type = BooleanType }.get().set(
+                single().className.toClass(initialize = true).resolve().apply {
+                    firstField {
+                        name = single().fieldName
+                        type = Boolean::class
+                    }.set(
                         when (redMode) {
                             "1" -> true
                             "2" -> false
