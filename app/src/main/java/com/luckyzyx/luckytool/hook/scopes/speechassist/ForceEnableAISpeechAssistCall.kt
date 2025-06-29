020c29c5d34fce81d0c06cb89220c5f1258e5278
luckyzyx <2813467705@qq.com>
2025-06-29 15:26:12 +0800

    迁移KavaRef API speechassist

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/speechassist/ForceEnableAISpeechAssistCall.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/speechassist/ForceEnableAISpeechAssistCall.kt
@@ -1,18 +1,18 @@
 package com.luckyzyx.luckytool.hook.scopes.speechassist
 
+import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.method
 import org.lsposed.lsparanoid.Obfuscate
 
 @Obfuscate
 object ForceEnableAISpeechAssistCall : YukiBaseHooker() {
     override fun onHook() {
         //Source AiCallCommonBean
-        "com.heytap.speechassist.aicall.setting.config.AiCallCommonBean".toClass().apply {
-            method { name = "getSupportAiCall" }.hook {
+        "com.heytap.speechassist.aicall.setting.config.AiCallCommonBean".toClass().resolve().apply {
+            firstMethod { name = "getSupportAiCall" }.hook {
                 replaceToTrue()
             }
-            method { name = "getSupportAiCallV2" }.hook {
+            firstMethod { name = "getSupportAiCallV2" }.hook {
                 replaceToTrue()
             }
         }
