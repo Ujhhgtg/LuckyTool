925d85929ae23f5e5c255f74ff1fef7e4ffbec39
luckyzyx <2813467705@qq.com>
2025-06-29 15:25:43 +0800

    迁移KavaRef API soundrecorder

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/soundrecorder/HookBaseUtil.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/soundrecorder/HookBaseUtil.kt
@@ -1,15 +1,15 @@
 package com.luckyzyx.luckytool.hook.scopes.soundrecorder
 
+import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.method
 import org.lsposed.lsparanoid.Obfuscate
 
 @Obfuscate
 object HookBaseUtil : YukiBaseHooker() {
     override fun onHook() {
         //Source BaseUtil
-        "com.soundrecorder.base.utils.BaseUtil".toClass().apply {
-            method { name = "isRealme" }.hook {
+        "com.soundrecorder.base.utils.BaseUtil".toClass().resolve().apply {
+            firstMethod { name = "isRealme" }.hook {
                 replaceToTrue()
             }
         }
