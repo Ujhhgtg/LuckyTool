6b844510fe8e72a6416a1c1d0461f28e896e947b
luckyzyx <2813467705@qq.com>
2025-07-27 19:36:42 +0800

    优化sn判断isNotBlank

--- app/src/main/java/com/luckyzyx/luckytool/service/GlobalFuncService.kt
+++ app/src/main/java/com/luckyzyx/luckytool/service/GlobalFuncService.kt
@@ -60,7 +60,7 @@ object GlobalFuncService : BaseControllerService<IGlobalFuncController>() {
             override fun getSnInfo(): String {
                 val sn = SystemProperties.get("ro.serialno", "")
                 val chipid = SystemProperties.get("ro.boot.chipid")
-                return listOf(sn, chipid).filter { it.isNotEmpty() }  // 过滤掉空字符串
+                return listOf(sn, chipid).filter { it.isNotBlank() }  // 过滤掉空字符串
                     .minByOrNull { it.length }  // 找出最短的
                     ?: "null"  // 如果全部为空，则返回空字符串
             }
--- app/src/main/java/com/luckyzyx/luckytool/utils/DeviceUtils.kt
+++ app/src/main/java/com/luckyzyx/luckytool/utils/DeviceUtils.kt
@@ -121,7 +121,7 @@ object DeviceUtils {
         val sn = ShellUtils.fastCmd(command1)
         val command2 = "getprop ro.boot.chipid"
         val chipid = ShellUtils.fastCmd(command2)
-        return listOf(sn, chipid).filter { it.isNotEmpty() }  // 过滤掉空字符串
+        return listOf(sn, chipid).filter { it.isNotBlank() }  // 过滤掉空字符串
             .minByOrNull { it.length }  // 找出最短的
             ?: "null"  // 如果全部为空，则返回空字符串
     }
