6811e81418fb187dfebc0aa9ff7f8c63f450620a
luckyzyx <2813467705@qq.com>
2025-12-27 22:33:02 +0800

    Hook alternative checkDowngrade method on A15+ [https://github.com/LSPosed/CorePatch/commit/b1007e9cd3720d5db860fbd0090784e4e79f3661]

--- app/src/main/java/com/luckyzyx/luckytool/hook/CorePatch/CorePatchForV.java
+++ app/src/main/java/com/luckyzyx/luckytool/hook/CorePatch/CorePatchForV.java
@@ -2,10 +2,27 @@ package com.luckyzyx.luckytool.hook.CorePatch;
 
 import org.lsposed.lsparanoid.Obfuscate;
 
+import java.lang.reflect.InvocationTargetException;
+
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 @Obfuscate
 public class CorePatchForV extends CorePatchForU {
+    
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        super.handleLoadPackage(loadPackageParam);
+        
+        var checkDowngradeAlt = XposedHelpers.findMethodExactIfExists("com.android.server.pm.PackageManagerServiceUtils",
+                loadPackageParam.classLoader, "checkDowngrade", "com.android.server.pm.PackageSetting",
+                "android.content.pm.PackageInfoLite");
+        if (checkDowngradeAlt != null) {
+            XposedBridge.hookMethod(checkDowngradeAlt, new ReturnConstant(prefs, "downgrade", null));
+        }
+    }
+    
     @Override
     Class<?> getParsedPackage(ClassLoader classLoader) {
         return XposedHelpers.findClassIfExists("com.android.internal.pm.parsing.pkg.ParsedPackage", classLoader);
