1a88cb5162bfb8ec6985ae606bb113d9cb320a23
luckyzyx <2813467705@qq.com>
2026-04-01 07:33:17 +0800

    Revert "feat: 移除哔哩开屏广告"

--- app/src/main/java/com/luckyzyx/luckytool/ui/fragment/scopes/XposedFragment.kt
+++ app/src/main/java/com/luckyzyx/luckytool/ui/fragment/scopes/XposedFragment.kt
@@ -73,7 +73,6 @@ import com.luckyzyx.luckytool.ui.fragment.scopes.apps.OplusWeather
 import com.luckyzyx.luckytool.ui.fragment.scopes.apps.OplusWirelessSettings
 import com.luckyzyx.luckytool.ui.fragment.scopes.others.ADM
 import com.luckyzyx.luckytool.ui.fragment.scopes.others.AlphaBackupPro
-import com.luckyzyx.luckytool.ui.fragment.scopes.others.Bili
 import com.luckyzyx.luckytool.ui.fragment.scopes.others.KsWeb
 import com.luckyzyx.luckytool.ui.fragment.scopes.related.AndroidRelated
 import com.luckyzyx.luckytool.ui.fragment.scopes.related.AodRelated
@@ -223,7 +222,6 @@ class XposedFragment : BaseScopePreferenceFeagment(), MenuProvider {
         addFragmentPreference(this, allPrefs, AlphaBackupPro())
         addFragmentPreference(this, allPrefs, KsWeb())
         addFragmentPreference(this, allPrefs, ADM())
-        addFragmentPreference(this, allPrefs, Bili())
 
         return allPrefs
     }
@@ -310,7 +308,6 @@ class XposedFragment : BaseScopePreferenceFeagment(), MenuProvider {
         addFragmentItem(context, allFragmentItem, ADM())
         addFragmentItem(context, allFragmentItem, AlphaBackupPro())
         addFragmentItem(context, allFragmentItem, KsWeb())
-        addFragmentItem(context, allFragmentItem, Bili())
 
     }
 
