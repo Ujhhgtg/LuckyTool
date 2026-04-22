c7fabc5eb54400053043c7797b27cc83f0e880d8
luckyzyx <2813467705@qq.com>
2026-04-22 12:23:32 +0800

    fix: 移除桌面图标徽标 CacheUtils

--- app/src/main/java/com/luckyzyx/luckytool/hook/scopes/launcher/HookAppBadge.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scopes/launcher/HookAppBadge.kt
@@ -1,10 +1,10 @@
 package com.luckyzyx.luckytool.hook.scopes.launcher
 
+import android.graphics.drawable.Drawable
+import android.os.UserHandle
 import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.luckyzyx.luckytool.utils.A14
 import com.luckyzyx.luckytool.utils.ModulePrefs
-import com.luckyzyx.luckytool.utils.SDK
 import com.luckyzyx.luckytool.utils.getOSVersionCode
 import org.lsposed.lsparanoid.Obfuscate
 
@@ -12,7 +12,7 @@ import org.lsposed.lsparanoid.Obfuscate
 object HookAppBadge : YukiBaseHooker() {
     override fun onHook() {
         val osCode = getOSVersionCode
-        if (SDK >= A14) loadHooker(AppBadge) else loadHooker(AppBadgeC13)
+        if (osCode >= 30) loadHooker(AppBadge) else loadHooker(AppBadgeC13)
     }
 
     @Obfuscate
@@ -37,20 +37,41 @@ object HookAppBadge : YukiBaseHooker() {
                         if ((drawableCreationFlags and 2) == 0) {
                             if (badgeInfo != null) {
                                 if (isShortcut) resultNull()
-                            } else if ((flag and 2) != 0) {
+                            }
+                            if ((flag and 2) != 0) {
                                 //ic_instant_app_badge
                                 //resultNull()
-                            } else if ((flag and 4) != 0) {
-                                //ic_oplus_clone_app_badge
-                                if (isClone) resultNull()
-                            } else if ((flag and 1) != 0) {
-                                //ic_work_app_badge
-                                if (isWork) resultNull()
-                            } else if ((flag and 4) != 0) {
-                                //ic_clone_app_badge
+                            }
+                            if ((flag and 16) != 0) {
+                                //ic_archive_app_badge
+                                //resultNull()
+                            }
+                            if ((flag and 4) == 0) {
+                                if ((flag and 1) != 0) {
+                                    //ic_work_app_badge
+                                    if (isWork) resultNull()
+                                } else if ((flag and 4) != 0) {
+                                    //ic_clone_app_badge
+                                    if (isClone) resultNull()
+                                }
+                            } else {
+                                //ic_oplus_clone_app_badge_new
                                 if (isClone) resultNull()
                             }
                         }
+                    }
+                }
+            }
+
+            //Source CacheUtils
+            "com.android.common.util.CacheUtils".toClass().resolve().apply {
+                firstMethod {
+                    name = "getCloneAppDrawable"
+                    parameters(UserHandle::class)
+                    returnType = Drawable::class
+                }.hook {
+                    after {
+                        if (isClone) resultNull()
                     }
                 }
             }
@@ -66,7 +87,7 @@ object HookAppBadge : YukiBaseHooker() {
 
             //Source BitmapInfo
             "com.android.launcher3.icons.BitmapInfo".toClass().resolve().apply {
-                firstMethod { name = "applyFlags";parameterCount = 3 }.hook {
+                firstMethod { name = "applyFlags"; parameterCount = 3 }.hook {
                     before {
                         val drawableCreationFlags = args().last().int()
                         val badgeInfo = firstField { name = "badgeInfo" }.of(instance).get()
