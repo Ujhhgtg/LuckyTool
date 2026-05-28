9365eaaa708658acf1b66b1da4fd71c9e1fede03
luckyzyx <2813467705@qq.com>
2026-05-28 21:44:03 +0800

    chore: Remove redundant signature configuration

--- colorpicker/build.gradle.kts
+++ colorpicker/build.gradle.kts
@@ -9,19 +9,6 @@ android {
 //            minorApiLevel = 1
         }
     }
-    defaultConfig {
-        minSdk = rootProject.extra.get("minSdkVersion") as Int
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
